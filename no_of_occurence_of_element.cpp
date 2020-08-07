a156e49c6d5c1852a438640ae6ba818631d53029
sagar <sagar.ramdev173@gmail.com>
2020-08-07 15:52:49 +0530

    total no of occurence of element in an sorted array

---
+++ no_of_occurence_of_element.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#define fo(i,n) for(int i=0;i<=n;i++)
+using namespace std;
+// program to find the Count number of occurrences (or frequency) in a sorted array
+int first_occurence_search(int a[],int item,int start,int end)
+{
+    int res=-1;
+    while(end>=start)
+    {
+        int mid=start+(end-start)/2;
+        if(a[mid]==item)
+            {
+                res=mid;                
+                end=mid-1;   // for first occurence of element 
+            }
+        else if(a[mid]>item)
+                end=mid-1;
+        else        
+            start=mid+1;            
+    }
+     return res;
+}
+int last_occurence_search(int a[],int item,int start,int end)
+{
+    int res=-1;
+    while(end>=start)
+    {
+        int mid=start+(end-start)/2;
+        if(a[mid]==item)
+            {
+                res=mid;
+                start=mid+1; // for last occurence of element
+                // end=mid-1;   // for first occurence of element 
+            }
+        else if(a[mid]>item)
+                end=mid-1;
+        else     
+            start=mid+1;        
+    }
+     return res;
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[]={2,3,4,5,6,7,8,9,10,10,10,11,11,11,11,11,11,12,13,14,19,20,25,33};    
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int item=11;    
+    int res=last_occurence_search(arr,item,0,n-1)-first_occurence_search(arr,item,0,n-1)+1; // to get Count number of occurrences (or frequency) in a sorted array    cout<<res<<endl;
+    return 0;
+}
